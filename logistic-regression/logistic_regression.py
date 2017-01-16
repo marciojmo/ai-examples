98fd082a8eb516f10607bdb6d3c3679c8befcbbf
Marcio Oliveira <Marcio Oliveira>
2017-01-16 02:54:25 -0200

    Finished optimization of logistic_regression using fmin_cg

--- logistic-regression/logistic_regression.py
+++ logistic-regression/logistic_regression.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 """
-A multi variate logistic regression model.
+A multi variate logistic regression model using scipy fmin_cg
 """
 
 import numpy as np
-from scipy.optimize import fmin_bfgs
+from scipy.optimize import fmin_bfgs, fmin_cg, minimize
 from matplotlib import pyplot as plt
 
 
@@ -18,43 +18,66 @@ def sigmoid(z):
     return g
 
 
-def cost_function(x, y, theta):
+def cost_function(theta, x, y):
     """
     Compute cost and gradient for logistic regression
     Computes the cost of using theta as the parameter for logistic regression.
-    This function also returns the gradient of the cost function in order to
-    use a 3rd party optimization function.
+    Obs: Theta must be the first parameter due to the optimization function
+    :param theta: The theta params
     :param x: The training data
     :param y: The output of this training data
-    :param theta: The theta params
     :return: The cost and gradient for the given theta
     """
-    m = y.shape[0]  # number of training examples
+    m, n = x.shape  # number of training examples and features
+    theta = np.reshape(theta, (n, 1))  # we need to reshape theta
     h = sigmoid(x * theta)  # hypothesis
     # logistic cost using the sigmoid function
     j = (-np.transpose(y) * np.log(h) - np.transpose(1 - y) * np.log(1 - h)) / m
     # logistic gradient for the given step (partial derivative)
-    grad = (np.transpose(x) * (h - y)) / m
+    # grad = (np.transpose(x) * (h - y)) / m
+    return j
 
-    return j, grad
 
+def predict(x,theta):
+    """
+    Predict whether the label is 0 or 1 using learned logistic regression parameters theta.
+    Computes the predictions for X using a threshold at 0.5 (i.e., if sigmoid(theta'*x) >= 0.5, predict 1)
+    :param x: The input data
+    :param theta: The params
+    :return: The class of the input
+    """
+    m, n = x.shape  # number of training examples and features
+    theta = np.reshape(theta, (n, 1))  # we need to reshape theta
+    z = x * theta
+    return z >= 0
 
 if __name__ == '__main__':
     print('Loading data...')
     data = np.loadtxt('data.txt', delimiter=',')
     x = np.matrix(data[:, 0:-1])  # input
     y = np.transpose(np.matrix(data[:, -1]))  # output
-    m = y.shape[0]  # number of training examples
+    m, n = x.shape
 
     # Plotting data
     print('Plotting data...')
-    pos = np.where( y == 1 )
-    neg = np.where( y == 0 )
-
-    plt.plot( x[pos,0], x[pos,1], 'r+', label='Admitted' )
-    plt.plot( x[neg,0], x[neg,1], 'bo', label='Not admitted' )
+    pos = np.where(y == 1)
+    neg = np.where(y == 0)
+    plt.plot(x[pos, 0], x[pos, 1], 'r+', label='Admitted')
+    plt.plot(x[neg, 0], x[neg, 1], 'bo', label='Not admitted')
     plt.xlabel('Exam 1 score')
     plt.ylabel('Exam 2 score')
-    #plt.legend(['Admitted', 'Not Admitted'])
-
+    plt.legend(('Admitted', 'Not Admitted'))
     plt.show()
+
+    # Adds the extra term
+    x = np.hstack((np.ones((m, 1)), x))
+
+    # Optimizes the parameters
+    initial_theta = np.zeros(n + 1)
+    theta = fmin_cg(cost_function, initial_theta, args=(x, y))
+
+    # Predicts the class of an arbitrary element (should be 0/False according to the training set)
+    x_test = np.matrix([1,0.96141,0.085526 ])
+    print(predict(x_test,theta))
+
+
